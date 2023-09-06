f9dc7b6038f8d944915ee1b0dbb9ae3efd62ced9
DenisPushka <92212786+DenisPushka@users.noreply.github.com>
2023-09-06 18:33:39 +0400

    + Create new Header.css (don't finish) # Update Header.jsx   + add "burger": show and hide

--- Сohabitation/App/src/Components/Header/Header.jsx
+++ Сohabitation/App/src/Components/Header/Header.jsx
@@ -1,42 +1,92 @@
-import bm from './ButtonMain.module.css';
-import top from './Top.module.css';
-import head from './Header.module.css';
 import {Nav} from "react-bootstrap";
 import {Component} from "react";
+import "./NewHeader.css";
 
 // Шапка.
 class Header extends Component {
 
     constructor(props) {
         super(props);
+
+        this.showLittleMenu = this.showLittleMenu.bind(this);
+        this.checkOpenMenu = this.checkOpenMenu.bind(this);
+    }
+
+    /**
+     * Показывает меню при нажатии на "гамбургер".
+     */
+    async showLittleMenu() {
+        let openDropdown = document.getElementById('navId');
+
+        openDropdown.className = openDropdown.className !== 'show' ? 'show' : 'hide';
+
+        if (openDropdown.className === 'show') {
+            window.setTimeout(function () {
+                openDropdown.style.opacity = 1;
+                openDropdown.style.transform = 'translateY(0px)';
+            }, 0);
+        } else if (openDropdown.className === 'hide') {
+            openDropdown.style.opacity = 0.2;
+            openDropdown.style.transform = 'translateY(-500px)';
+        }
+    }
+
+    /**
+     * Проверка на скрытие "гамбургера".
+     */
+    async checkOpenMenu() {
+        window.onclick = (event: MouseEvent) => {
+            if (!event.target.matches('.hamburger')) {
+                let openDropdown = document.getElementById('navId');
+                if (openDropdown.className === 'show') {
+                    openDropdown.style.opacity = 0;
+                    openDropdown.style.transform = 'translateY(-500px)';
+                    openDropdown.className = 'hide';
+                } else {
+                    openDropdown.style.opacity = 0.2;
+                    openDropdown.style.transform = 'translateY(-500px)';
+                }
+            }
+        };
+    }
+    
+    updateSize() {
+        let widthOutput = document.querySelector("#navId");
+        let mql = window.matchMedia("(min-width: 820px)");
+        if ((mql.matches) && (widthOutput.className !== 'nav')){
+            widthOutput.className = 'nav';
+            widthOutput.style.opacity = null;
+            widthOutput.style.transform = null;
+            // queue: true;
+        }
     }
 
     render() {
         return (<>
-            <header className={head.header}>
-                <img className={head.img_wrap} src={"test_pictur_two.jpg"}/>
+            <header className={"newHeader"} onClick={this.checkOpenMenu}>
+                <nav>
 
-                <div className={top.top}>
-                    <div className={top.topLeft}>
-                        <Nav.Link href="*"> Cohabitation</Nav.Link>
+                    <div>
+                        Name
                     </div>
 
-                    <div className={top.topRight}>
-                        <Nav.Link href="LogIn">Вход </Nav.Link>
-                        <Nav.Link href="SignUp">Регистрация</Nav.Link>
+                    <div class="hamburger menu-toggle" onClick={this.showLittleMenu}>
+                        <div class="line"></div>
+                        <div class="line"></div>
+                        <div class="line"></div>
                     </div>
-                </div>
 
-                {/*<div className="centered"> Мы поможем с соседом</div>*/}
+                    <ul class="nav" id="navId">
+                        <li><Nav.Link href="/#">Главная</Nav.Link></li>
+                        <li><Nav.Link href="/ProfileUser">Профиль</Nav.Link></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
 
-                <button type="button" className={`${head.button} ${bm.fill}`}>
-                    Фильтр
-                </button>
-
+                </nav>
             </header>
         </>);
     }
 }
-
 
 export default Header;
